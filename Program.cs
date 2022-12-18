31b8ff28c2ccd5333f61d0cf9066baa5b21896bd
Ronen <RonenNess@gmail.com>
2022-12-18 14:02:04 +0200

    .

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using PhotinoNET;
+using System.Drawing;
+
+namespace HelloPhotino
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string windowTitle = "Gamepad Test";
+            var window = new PhotinoWindow()
+                .SetTitle(windowTitle)
+                .SetUseOsDefaultSize(false)
+                .SetSize(new Size(700, 525))
+                .Center()
+                .SetResizable(false)
+                .Load("wwwroot/index.html"); // <-- can also try with "https://gamepad-tester.com/"
+            window.WaitForClose();
+        }
+    }
+}
